455b3c0ce1ffa300cf2245cf1fbadc096e943738
Zeynep Zeytun <zeynepzeytun.2002@gmail.com>
2025-07-28 21:52:22 +0300

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from src.ML_Project_w_MLFlow import logger
+from src.ML_Project_w_MLFlow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("welcome to our custom logging")
+STAGE_NAME= "Data Ingestion Stage"
+
+
+
+try:
+        logger.info(f">>>>>>>>>>>> stage {STAGE_NAME} started<<<<<<<<<")
+        obj=DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f"completed")
+except Exception as e:
+        logger.exception(e)
+        raise e
